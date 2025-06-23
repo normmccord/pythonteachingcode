741c2184aea0c45cc86e5e663e5918254ab48f8c
Norm McCord <nmccord3@students.kennesaw.edu>
2025-06-22 22:23:32 -0500

    minor edits

--- Python Absolute Beginner/p1m2normanmccord.py
+++ Python Absolute Beginner/p1m2normanmccord.py
@@ -1,6 +1,8 @@
+# P1M2 Norman McCord
+
 # [ ] create, call and test fishstore() function 
 def fishstore(fish, price):
-    return f"The fish you're selling is a {fish} and you want ${price} for it? Wow!!!"
+    return f"Norman McCord, the fish you're selling is a {fish} and you want ${price} for it? Wow!!!"
 
 fish_entry = input("What fish are you selling: ")
 price_entry = input("And what's the price: ")
